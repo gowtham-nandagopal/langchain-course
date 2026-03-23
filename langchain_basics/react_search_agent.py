b81b2d52237b242061be182d41101afee1a6d50a
Gowtham Nandagopal <gowthamn@Gowthams-Laptop.local>
2026-03-23 14:47:49 +0530

    Added the structured response

--- langchain_basics/react_search_agent.py
+++ langchain_basics/react_search_agent.py
@@ -1,7 +1,11 @@
 from dotenv import load_dotenv
 import os
+
+from langchain_google_genai import ChatGoogleGenerativeAI
 load_dotenv()
 
+from typing import List
+from pydantic import BaseModel, Field
 from langchain.agents import create_agent
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage
@@ -9,6 +13,19 @@ from langchain_groq import ChatGroq
 from tavily import TavilyClient
 
 tavily = TavilyClient()
+
+class Source(BaseModel):
+    """
+    Schema for a source used in the agent's response, including the URL of the source.
+    """
+    url: str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """
+    Schema for the agent's response, including the final answer and the sources used to arrive at that answer.
+    """
+    answer: str = Field(description="The final answer to the user's query, based on the content and sources")
+    sources: List[Source] = Field(default_factory=list,description="A list of sources used to arrive at the final answer")
 
 @tool
 def search(query: str) -> str:
@@ -24,12 +41,14 @@ def search(query: str) -> str:
     print(f"Searching for: {query}")
     return tavily.search(query=query)
 
-llm = ChatGroq(model="qwen/qwen3-32b", temperature=0)
+# llm = ChatGroq(model="qwen/qwen3-32b", temperature=0)
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite", temperature=1.0)
 tools = [search]
 
 agent = create_agent(
     model=llm,
     tools=tools,
+    response_format=AgentResponse,
     system_prompt="You are a helpful assistant that can use tools to answer questions. Always use the search tool for current information."
 )
 
